cc3347cf9e60e742334d366142a27b1c2b108815
jyi2ya <jyi2ya@outlook.com>
2023-09-15 22:11:12 +0800

    feat: application draft layout

--- src/app.rs
+++ src/app.rs
@@ -4,6 +4,7 @@
 pub struct RSSucks {
     // Example stuff:
     label: String,
+    list_unread_only: bool,
 
     // this how you opt-out of serialization of a member
     #[serde(skip)]
@@ -15,6 +16,7 @@ impl Default for RSSucks {
         Self {
             // Example stuff:
             label: "Hello World!".to_owned(),
+            list_unread_only: false,
             value: 2.7,
         }
     }
@@ -62,7 +64,11 @@ impl eframe::App for RSSucks {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self { label, value } = self;
+        let Self {
+            label,
+            list_unread_only,
+            value,
+        } = self;
 
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
@@ -82,44 +88,57 @@ impl eframe::App for RSSucks {
         });
 
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
+            ui.heading("Rust Sucks");
+            ui.label("用 Rust 写的 RSS 阅读器");
+            ui.label("虽然还不能用但是给我个 Star 好不好就当投资了嘛");
+            ui.hyperlink_to("RSSucks on Github", "https://github.com/jyi2ya/RSSucks");
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
+            ui.separator();
 
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
+            ui.button("今日订阅");
+            ui.button("等下再看");
+            ui.button("我的收藏");
 
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to(
-                        "eframe",
-                        "https://github.com/emilk/egui/tree/master/crates/eframe",
-                    );
-                    ui.label(".");
+            ui.separator();
+
+            ui.label("订阅列表");
+
+            let categories = vec![
+                "好看的",
+                "也不是不能看的",
+                "不好看的",
+                "难看的",
+                "浪费手机资源的",
+            ];
+
+            for category in categories {
+                ui.collapsing(category, |ui| {
+                    for i in 0..3 {
+                        ui.button(format!("{}-{}", category, i));
+                    }
                 });
-            });
+            }
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
+            ui.heading("订阅分类或者订阅本身的标题");
+            ui.label("一些关于订阅或者分类的介绍 blablablabla");
 
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-            ui.label("我们能支持中文吗？");
-            egui::warn_if_debug_build(ui);
+            ui.spacing();
+
+            ui.horizontal(|ui| {
+                ui.selectable_value(&mut self.list_unread_only, true, "未读");
+                ui.selectable_value(&mut self.list_unread_only, false, "所有");
+            });
+            ui.separator();
+
+            if self.list_unread_only {
+                ui.label("仅列出未读");
+                ui.label("这下面可能还需要列一堆订阅的文章、题图和摘要出来。可能要写个新的控件，先摆了总之");
+            } else {
+                ui.label("列出所有文章");
+                ui.label("这下面可能还需要列一堆订阅的文章、题图和摘要出来。可能要写个新的控件，先摆了总之");
+            }
         });
 
         if false {
